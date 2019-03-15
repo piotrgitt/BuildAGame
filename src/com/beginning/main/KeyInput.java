a17f4eb148baeb52ffd5e5ebc8da1b9336ea8c74
Piotr <Piotr@192.168.254.101>
2019-03-15 22:49:10 +0100

    Build a game - Step 10a Fixing key glitch - now Player doesn't stutter if fast going UP or DOWN, or LEFT RIGHT

--- src/com/beginning/main/KeyInput.java
+++ src/com/beginning/main/KeyInput.java
@@ -4,12 +4,19 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter {
+    private boolean[] keyDown = new boolean[4];
+    
     private Handler handler;
     public GameObject tempObject;//just an teporary object
     public int tempVel;//just an tempoary variable
     //Constructor
     public KeyInput(Handler handler){
         this.handler = handler;
+        
+        keyDown[0] = false; // UP - key
+        keyDown[1] = false; // DOWN - key
+        keyDown[2] = false; // RIGHT - key
+        keyDown[3] = false; // LEFT - key
     }
     
     
@@ -25,16 +32,20 @@ public class KeyInput extends KeyAdapter {
              */
             if (tempObject.getId() == ID.Player ){
                 if(key == KeyEvent.VK_UP){
-                    tempObject.setVelY(-3);
+                    tempObject.setVelY(-4);
+                    keyDown[0] = true;
                 }
                 if(key == KeyEvent.VK_DOWN){
-                    tempObject.setVelY(3);
+                    tempObject.setVelY(4);
+                    keyDown[1] = true;
                 }
                 if(key == KeyEvent.VK_RIGHT){
-                    tempObject.setVelX(3);
+                    tempObject.setVelX(4);
+                    keyDown[2] = true;
                 }
                 if(key == KeyEvent.VK_LEFT){
-                    tempObject.setVelX(-3);
+                    tempObject.setVelX(-4);
+                    keyDown[3] = true;
                 }
             }
             
@@ -55,17 +66,27 @@ public class KeyInput extends KeyAdapter {
              */
             if (tempObject.getId() == ID.Player ){
                 if(key == KeyEvent.VK_UP){
-                    tempObject.setVelY(0);
+                    //tempObject.setVelY(0);
+                    keyDown[0] = false;
                 }
                 if(key == KeyEvent.VK_DOWN){
-                    tempObject.setVelY(0);
+                    //tempObject.setVelY(0);
+                    keyDown[1] = false;
                 }
                 if(key == KeyEvent.VK_RIGHT){
-                    tempObject.setVelX(0);
+                    //tempObject.setVelX(0);
+                    keyDown[2] = false;
                 }
                 if(key == KeyEvent.VK_LEFT){
-                    tempObject.setVelX(0);
+                    //tempObject.setVelX(0);
+                    keyDown[3] = false;
                 }
+                
+                //vertical movement (up, down)
+                if(keyDown[0] && keyDown[1] == false) tempObject.setVelY(0);
+                
+                //horizontal movement(left, right)
+                if(keyDown[2] & keyDown[3] == false) tempObject.setVelX(0);
             }
             
         }
